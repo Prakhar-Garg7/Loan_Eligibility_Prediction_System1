90d2aacebda70009cc2c599c890903fd97848a90
Prakhar <prakhargarg1405@gmail.com>
2024-05-21 22:34:05 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,37 @@
+import numpy as np
+from flask import Flask, request, render_template
+import pickle
+from sklearn.metrics import classification_report
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+
+app = Flask(__name__)
+
+model = pickle.load(open('models/model.pkl', 'rb'))
+
+@app.route('/')
+def home() :
+                    return render_template('index.html')
+
+@app.route('/predict', methods = ['POST'])
+def predict():
+                    features = list(request.form.values())
+                    for i in range ( 0, 5 ) :
+                                        features[i] = int(features[i])
+                    for i in range ( 5, 10 ) :
+                                        features[i] = float(features[i])
+                    features[10] = int(features[i])
+                    features = np.array( features )
+                    features = features.reshape(1, -1)
+                    prediction = model.predict( features )
+                    
+                    return render_template('index.html', prediction_text = 'loan approved or not {}' .format(prediction) )
+
+if __name__ == "__main__":
+                    app.run(debug=True)
+                    
+                    
+#change
